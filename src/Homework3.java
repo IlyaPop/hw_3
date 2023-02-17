8ef7265e1859bd9d5782cb787f95b5d27dc55f45
79098 <ilya.nesterov1520@gmail.com>
2023-02-17 17:47:56 +1000

    hw done

---
+++ src/Homework3.java
@@ -0,0 +1,47 @@
+
+public class Homework3 {
+    public static void main(String[] args) {
+        System.out.println("ZADANIE 1");
+        double dog = 8;
+        double cat = 3.6;
+        int paper = 763789;
+        System.out.println("dog = " + dog + "; cat = " + cat + "; paper = " + paper + ";");
+
+        System.out.println("ZADANIE 2");
+        dog = dog * 4;
+        cat = cat * 4;
+        paper = paper * 4;
+        System.out.println("dog =  " + dog + "; cat = " + cat + "; paper = " + paper + ";");
+
+        System.out.println("ZADANIE 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog = " + dog + "; cat = " + cat + "; paper = " + paper + ";");
+
+        System.out.println("ZADANIE 4");
+        int friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("zdanie 5");
+        double frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        System.out.println("zdanie 6");
+        double b1 = 78.2;
+        double b2 = 82.7;
+        System.out.println("Общий вес: " + (b1 + b2));
+        double diff = b2 - b1;
+        System.out.println("Разница : " + diff);
+    }
+}
